6f182ec4ad987f81ad690911728e186323e63dfd
Mikael Koch Drewes <IMT-A-MIDRRA@regionh.dk>
2022-04-22 02:59:17 +0200

    Completed module

--- index.js
+++ index.js
@@ -2,7 +2,24 @@ import express from 'express';
 import bodyParser from 'body-parser';
 
 const app = express();
-const PORT = 4000;
+const PORT = 4005;
+const mongoose = require('mongoose');
+
+const url =
+'mongodb+srv://kovatoch:Consepi123@globomantics.localhost?retryWrites=true&w=majority';
+const dbName = 'productsdb';
+
+mongoose.Promise = global.Promise;
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+
+mongoose.connect('mongodb://localhost/productsdb');
+
+
+
 
 // bodyparser setup
 app.use(bodyParser.urlencoded({ extended: true }));
